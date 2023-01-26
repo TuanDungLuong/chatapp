a4df1ae9ba78cbdb440d20d90549536f837f1494
Lương Tuấn Dũng <107998520+TuanDungLuong@users.noreply.github.com>
2023-01-26 07:52:35 +0700

    s
    s

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -118,6 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 import os
-STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_URL = 'theme/static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'theme/static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
